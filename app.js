0003312f979f58e994c98679ec5bad806b75d59a
flyingfoxweb <erin@flyingfox.net.nz>
2018-01-12 02:35:42 +0000

    Added the show page template and to app.js to show the content of a post when the Read More button is clicked on at the /blogs page. Added styling.

--- app.js
+++ app.js
@@ -62,6 +62,16 @@ app.post("/blogs", function(req, res) {
     });
 });
 
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {blog: foundBlog})
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running..");
